2a7cca1fa166fde09aa57c2cfc57950ffb691e88
Sara Kamel <sarakamel591@gmail.com>
2024-06-23 12:02:30 -0700

    Build project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-
+import Header from './component/nav';
+import HeroSection from './component/hero-section';
+import Highlights from './component/highlights';
+import Testimonials from './component/testimonials';
+import About from './component/about';
+import Footer from './component/footer';
 function App() {
   return (
     <>
-      <h1>HomePage</h1>
+      <Header />
+      <main>
+        <HeroSection />
+        <Highlights />
+        <Testimonials />
+        <About />
+      </main>
+      <Footer />
     </>
   );
 }
