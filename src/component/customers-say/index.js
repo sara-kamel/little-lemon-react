4fff07d3aa05db74badd80ea060a880888bf8de4
Sara Kamel <sarakamel591@gmail.com>
2024-07-05 17:11:39 -0700

    Add Customer say section with responsive design

--- src/component/customers-say/index.js
+++ src/component/customers-say/index.js
@@ -1,3 +1,71 @@
+import { IoStarSharp } from 'react-icons/io5'
+import './styles.css'
 export default function CustomersSay () {
-  return <section></section>
+  return (
+    <section className='customers-say'>
+      <h1>Customers Say</h1>
+      <div className='reviews-section'>
+        <article>
+          <h2>
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+          </h2>
+          <img
+            src='https://as2.ftcdn.net/v2/jpg/04/04/84/35/1000_F_404843510_CVgjxWxhW2ipkOM8Vuk5IQEnc1Q54by6.jpg'
+            alt='Profile Picture'
+          />
+          <h2>Sophia Deem</h2>
+          <p>"Really enjoyed the atmosphere"</p>
+        </article>
+        <article>
+          <h2>
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+          </h2>
+          <img
+            src='https://as2.ftcdn.net/v2/jpg/01/87/61/95/1000_F_187619537_7DkUeyM8SnZJSRMaUNT6RdXOZgeZNLmL.jpg'
+            alt='Profile Picture'
+          />
+          <h2>Mark Makeen</h2>
+          <p>"The greek salad was excellent"</p>
+        </article>
+        <article>
+          <h2>
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+          </h2>
+          <img
+            src='https://as1.ftcdn.net/v2/jpg/02/95/75/36/1000_F_295753685_BHhugvDMOUu4YDwYxyuvdJIG2t0HXl5Z.jpg'
+            alt='Profile Picture'
+          />
+          <h2>Loura Jimmy</h2>
+          <p>"You have to try the Greek Salad"</p>
+        </article>
+        <article>
+          <h2>
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+            <IoStarSharp color='#f4ce14' />
+          </h2>
+          <img
+            src='https://as1.ftcdn.net/v2/jpg/01/77/13/92/1000_F_177139258_UFcopZmuwFUbpNJRJKfQ0xbdpyeoDwHX.jpg'
+            alt='Profile Picture'
+          />
+          <h2>Martina Luke</h2>
+          <p>"Awesome place peaceful atmosphere with delicious food!"</p>
+        </article>
+      </div>
+    </section>
+  )
 }
